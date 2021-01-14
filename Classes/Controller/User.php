972381c734546592021c934ddab9a13418977178
ahmed walleed <ahmedwalleed15@gmail.com>
2021-01-14 09:11:15 +0200

    start with varibles in user

---
+++ Classes/Controller/User.php
@@ -0,0 +1,13 @@
+<!-- userId	FirstName	SecName	userName	Email	Address	Password	Allowance	MemberShip	AnsOfSecurityQ	ReportId	CurrentCartId	 -->
+<?php
+class User
+{
+    private $userId;
+    private $FirstName;
+    private $SecName;
+    private $userName;
+    private $Email;
+    private $Address;
+    private $Allowance;
+    
+}
