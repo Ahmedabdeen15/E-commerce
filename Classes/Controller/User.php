af14f4d00f42fa2ace98734b70f5423de0164515
ahmed walleed <ahmedwalleed15@gmail.com>
2021-01-14 15:09:11 +0200

    create login fn

--- Classes/Controller/User.php
+++ Classes/Controller/User.php
@@ -1,5 +1,6 @@
 <!-- userId	FirstName	SecName	userName	Email	Address	Password	Allowance	MemberShip	AnsOfSecurityQ	ReportId	CurrentCartId	 -->
 <?php
+include 'C:\xampp\htdocs\project\Classes\Database\User_db.php';
 class User
 {
     private $userId;
@@ -17,5 +18,23 @@ class User
     {
         $this->user_db=new User_db();
     }
-    
+    function login($User,$password)
+    {
+        if($this->user_db->checklogin($User,$password))
+        {
+            $row=$this->user_db->checklogin($User,$password);
+            $this->setObject($row);
+        }else{echo "no data";}
+    }
+    private function setObject($row)
+    {
+        foreach($row as $key => $cont)
+        {
+            echo "<br>$key => $cont";
+            $this->$key=$cont;
+        }
+    }
 }
+// testing
+$x=new User();
+$x->login("ahmed","ahmed");
