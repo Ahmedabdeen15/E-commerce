4c843c4e789eb708aa9ac6781a0f72b88e0781d0
ahmed walleed <ahmedwalleed15@gmail.com>
2021-01-17 21:50:16 +0200

    add flag to know in you are user or admin

--- Classes/Viewer/login_serv.php
+++ Classes/Viewer/login_serv.php
@@ -1,8 +1,9 @@
 <?php
 include 'C:\xampp\htdocs\project\Classes\Controller\User.php';
 require_once 'C:\xampp\htdocs\project\Classes\Controller\Admin.php';
-
-
+session_start();
+session_unset();
+$_SESSION['flag']=0;
 if(isset($_POST['submit']))
 {
     // unset($_SESSION['user']);
@@ -11,16 +12,14 @@ if(isset($_POST['submit']))
     $Admin=new Admin();
     if($User->login($_POST['username'],$_POST['password']))
     {
-        session_start();
-        unset($_SESSION['user']);
-        unset($_SESSION['user_id']);
-        $_SESSION['user']=$User;
+        $_SESSION['flag']=0;
         $_SESSION['user_id']=$User->GetuserId();
         // echo '<br/>'.$_SESSION['user_id'].'<br/>';
         header("location: "."Index_d.php");
     }
     else if($Admin->login($_POST['username'],$_POST['password']))
     {
+        $_SESSION['flag']=1;
         $_SESSION['user']=$Admin;
         $_SESSION['user_id']=$Admin->GetuserId();
         header("location: "."Index_dd.php");
